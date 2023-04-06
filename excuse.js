73f2d8b73f85677c45348e446e77ac6f3e27e113
AlejandroRivera2306 <ale_jandro2306@hotmail.com>
2023-04-06 03:00:27 +0000

    CAMBIOS FINALES

--- excuse.js
+++ excuse.js
@@ -1,29 +1,10 @@
-function quien () {
+
     let who = ['the dog','my granma','his turtle','my bird'];
-    let aleatorio = who[(Math.floor(Math.random() * who.length))];
-    return aleatorio;
-  } 
-  
-  function action () {
     let action = ['eat','pissed','crushed','broked'];
-    let aleatorio2 = action[(Math.floor(Math.random() * action.length))];
-    return aleatorio2;
-  }
-
-  function cosa(){
-    let what = ['my homework', 'the keys', 'the car'];
-    let aleatorio4 = what[(Math.floor(Math.random() * what.length))];
-    return aleatorio4;
-
-  }
-  
-  function donde () {
+    let cosa = ['my homework', 'the keys', 'the car'];
     let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
-    let aleatorio3 = when[(Math.floor(Math.random() * when.length))];
-    return aleatorio3;
+  function random (lista){
+  return lista[(Math.floor(Math.random() * lista.length))];
   }
-  
-  let resultado = `${quien()} ${action()} ${cosa()} ${donde()}`;
-  //typeof (resultado)
+  let resultado = `${random(who)} ${random(action)} ${random(cosa)} ${random(when)}`;
   document.getElementById("impre").innerHTML = resultado;
-  
