1d1a1e26ff93db659c2d05c95f6432582ddb81f9
AlejandroRivera2306 <ale_jandro2306@hotmail.com>
2023-04-11 01:11:05 +0000

    Cambios finalesx

--- excuse.js
+++ excuse.js
@@ -1,10 +1,12 @@
 
-    let who = ['the dog','my granma','his turtle','my bird'];
-    let action = ['eat','pissed','crushed','broked'];
-    let cosa = ['my homework', 'the keys', 'the car'];
-    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
-  function random (lista){
-  return lista[(Math.floor(Math.random() * lista.length))];
-  }
-  let resultado = `${random(who)} ${random(action)} ${random(cosa)} ${random(when)}`;
-  document.getElementById("impre").innerHTML = resultado;
+let who = ['the dog','my granma','his turtle','my bird'];
+let action = ['eat','pissed','crushed','broked'];
+let item = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+function getRandomItem (list){
+  return list[(Math.floor(Math.random() * list.length))];
+}
+
+let result = `${getRandomItem(who)} ${getRandomItem(action)} ${getRandomItem(item)} ${getRandomItem(when)}`;
+document.getElementById("impre").innerHTML = result;
